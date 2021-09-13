56475bb357b6f89b07462cfa72b7c2bce6f5d0f5
Fabian Finalé Franqui <fabianfinale@gmail.com>
2021-09-13 17:00:13 -0400

    Build the basic layout

---
+++ src/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Layout = () => {
+  return (
+    <div>
+      {/* Header */}
+      {/* Red section */}
+      {/* Yellow section */}
+      {/* Perks */}
+      {/* Reviews */}
+      {/* Get EXP|CON now */}
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default Layout;

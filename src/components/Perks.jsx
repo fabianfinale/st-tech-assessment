73acccc1ab83c2c3004ad8e214c2c0b596981b97
Fabian Finalé Franqui <fabianfinale@gmail.com>
2021-09-20 10:44:08 -0400

    Build the Perks component

--- src/components/Perks.jsx
+++ src/components/Perks.jsx
@@ -2,7 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Perks = ({ reference }) => {
-  return <section className='perks-section' ref={reference}></section>;
+  return (
+    <section className='perks-section' ref={reference}>
+      <div className='section__content'>
+        <h2 className='section__heading uppercase'>Perks</h2>
+        <div className='section__listing'>
+          <div className='listing__group'>
+            <h3 className='listing__heading heading--red'>
+              Subscription Payment Model
+            </h3>
+            <p className='listing__text'>
+              No commitment, cancel anytime. Never worry about forgetting a
+              payment again!
+            </p>
+          </div>
+          <div className='listing__group'>
+            <h3 className='listing__heading heading--blue'>
+              No Fee Cancelation Policy
+            </h3>
+            <p className='listing__text'>
+              No commitment, cancel anytime. Never worry about forgetting a
+              payment again!
+            </p>
+          </div>
+          <div className='listing__group'>
+            <h3 className='listing__heading heading--yellow'>
+              Subscription Payment Model
+            </h3>
+            <p className='listing__text'>
+              No commitment, cancel anytime. Never worry about forgetting a
+              payment again!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 Perks.propTypes = {

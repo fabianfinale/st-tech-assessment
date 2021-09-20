0fdb06ea913e08d1bff151858bfaa68b24963b90
Fabian Finalé Franqui <fabianfinale@gmail.com>
2021-09-20 14:16:53 -0400

    Build the Footer component

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,19 @@
+import { Icon } from '@mui/material';
 import React from 'react';
 
 const Footer = () => {
-  return <footer className='footer'></footer>;
+  return (
+    <footer className='footer'>
+      <a className='footer__link' href='mailto:support@experienceconcerts.co'>
+        <Icon>mail</Icon>
+        support@experienceconcerts.co
+      </a>
+      <h2 className='footer__brand'>EXP|CON</h2>
+      <p className='footer__copyright'>
+        2019 &copy; All Rights Reserved | Speer Technologies Incorporated
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;

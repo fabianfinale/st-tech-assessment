4c7722f4b04bb3153ecbc1614f89e38f530854a4
Fabian Finalé Franqui <fabianfinale@gmail.com>
2021-09-20 12:47:27 -0400

    Build the Get It Now component

--- src/components/GetItNot.jsx
+++ src/components/GetItNot.jsx
@@ -2,7 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const GetItNow = ({ reference }) => {
-  return <section className='get-it-now-section' ref={reference}></section>;
+  return (
+    <section className='get-it-now-section' ref={reference}>
+      <div className='section__content'>
+        <h2 className='section__heading uppercase heading--red'>
+          Get EXP|CON now
+        </h2>
+        <p className='section__text heading--white'>
+          Purchase and download the app.
+        </p>
+      </div>
+      <div className='section__cta'>
+        <button className='btn btn--gradient' data-text='Try it now'>
+          Try it now
+        </button>
+      </div>
+    </section>
+  );
 };
 
 GetItNow.propTypes = {
